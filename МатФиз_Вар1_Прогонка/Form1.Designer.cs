5bdb8c5c12df214c5bb599296a02ae650992862d
Olgaselezneva28 <89875387376@mail.ru>
2022-06-21 15:56:04 +0300

    1.0

---
+++ МатФиз_Вар1_Прогонка/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace МатФиз_Вар1_Прогонка
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.очистииьГрафикToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.zedGraphControl2 = new ZedGraph.ZedGraphControl();
+            this.очиститьТаблицуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.очиститьГрафикКоэффициэнтовToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.очиститьВсеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.очистииьГрафикToolStripMenuItem,
+            this.очиститьТаблицуToolStripMenuItem,
+            this.очиститьГрафикКоэффициэнтовToolStripMenuItem,
+            this.очиститьВсеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1141, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // очистииьГрафикToolStripMenuItem
+            // 
+            this.очистииьГрафикToolStripMenuItem.Name = "очистииьГрафикToolStripMenuItem";
+            this.очистииьГрафикToolStripMenuItem.Size = new System.Drawing.Size(205, 24);
+            this.очистииьГрафикToolStripMenuItem.Text = "Очистииь график функции";
+            this.очистииьГрафикToolStripMenuItem.Click += new System.EventHandler(this.очистииьГрафикToolStripMenuItem_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(243, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "h = ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(292, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "0,01";
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(4, 7);
+            this.zedGraphControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(1098, 358);
+            this.zedGraphControl1.TabIndex = 6;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 71);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1117, 409);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.zedGraphControl1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1109, 380);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "График функции";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1109, 380);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Таблица ci и столбец fi";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1097, 368);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.zedGraphControl2);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1109, 380);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "График коэффициентов";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // zedGraphControl2
+            // 
+            this.zedGraphControl2.Location = new System.Drawing.Point(7, 7);
+            this.zedGraphControl2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl2.Name = "zedGraphControl2";
+            this.zedGraphControl2.ScrollGrace = 0D;
+            this.zedGraphControl2.ScrollMaxX = 0D;
+            this.zedGraphControl2.ScrollMaxY = 0D;
+            this.zedGraphControl2.ScrollMaxY2 = 0D;
+            this.zedGraphControl2.ScrollMinX = 0D;
+            this.zedGraphControl2.ScrollMinY = 0D;
+            this.zedGraphControl2.ScrollMinY2 = 0D;
+            this.zedGraphControl2.Size = new System.Drawing.Size(1095, 366);
+            this.zedGraphControl2.TabIndex = 0;
+            // 
+            // очиститьТаблицуToolStripMenuItem
+            // 
+            this.очиститьТаблицуToolStripMenuItem.Name = "очиститьТаблицуToolStripMenuItem";
+            this.очиститьТаблицуToolStripMenuItem.Size = new System.Drawing.Size(145, 24);
+            this.очиститьТаблицуToolStripMenuItem.Text = "Очистить таблицу";
+            this.очиститьТаблицуToolStripMenuItem.Click += new System.EventHandler(this.очиститьТаблицуToolStripMenuItem_Click);
+            // 
+            // очиститьГрафикКоэффициэнтовToolStripMenuItem
+            // 
+            this.очиститьГрафикКоэффициэнтовToolStripMenuItem.Name = "очиститьГрафикКоэффициэнтовToolStripMenuItem";
+            this.очиститьГрафикКоэффициэнтовToolStripMenuItem.Size = new System.Drawing.Size(251, 24);
+            this.очиститьГрафикКоэффициэнтовToolStripMenuItem.Text = "Очистить график коэффициэнтов";
+            this.очиститьГрафикКоэффициэнтовToolStripMenuItem.Click += new System.EventHandler(this.очиститьГрафикКоэффициэнтовToolStripMenuItem_Click);
+            // 
+            // очиститьВсеToolStripMenuItem
+            // 
+            this.очиститьВсеToolStripMenuItem.Name = "очиститьВсеToolStripMenuItem";
+            this.очиститьВсеToolStripMenuItem.Size = new System.Drawing.Size(112, 24);
+            this.очиститьВсеToolStripMenuItem.Text = "Очистить все";
+            this.очиститьВсеToolStripMenuItem.Click += new System.EventHandler(this.очиститьВсеToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1141, 491);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.ToolStripMenuItem очистииьГрафикToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabPage tabPage3;
+        private ZedGraph.ZedGraphControl zedGraphControl2;
+        private System.Windows.Forms.ToolStripMenuItem очиститьТаблицуToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem очиститьГрафикКоэффициэнтовToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem очиститьВсеToolStripMenuItem;
+    }
+}
+
---
+++ МатФиз_Вар1_Прогонка/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace МатФиз_Вар1_Прогонка
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Double[] Ci; //Набор точек - решений 
+        Double[] xi; //Набор точек сетки с шагом h
+        Int64 n; // число точек сетки 
+
+        //Прогонка
+        public void solveMatrix( Double[] a, Double[] c, Double[] b, Double[] f)
+        {
+            /*
+	         * b - диагональ, лежащая над главной (нумеруется: [0;n-2])
+	         * c - главная диагональ матрицы A (нумеруется: [0;n-1])
+	         * a - диагональ, лежащая под главной (нумеруется: [1;n-1])
+	         * f - правая часть (столбец)
+	         */
+            int n = f.Count(); 
+            double cmp;
+            for (int i = 1; i < n; i++)
+            {
+                cmp = a[i] / c[i - 1];
+                c[i] = c[i] - cmp * b[i - 1];
+                f[i] = f[i] - cmp * f[i - 1];
+            }
+
+            //
+            Ci[n - 1] = f[n - 1] / c[n - 1];
+
+            for (int i = n - 2; i >= 0; i--)
+            {
+                Ci[i] = (f[i] - b[i] * Ci[i + 1]) / c[i];
+            }
+        }
+
+        //Start
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Считаем шаг 
+            Double h = Convert.ToDouble(textBox1.Text);
+            
+            n = (Int64)(1 / h); // число точек сетки 
+
+            Ci = new Double[n + 1];
+            xi = new Double[n + 1];
+            Double[] fi = new Double[n + 1];
+            Double[] bi = new Double[n + 1];
+            Double[] ci = new Double[n + 1];
+            Double[] ai = new Double[n + 1];
+
+            //Формируем сетку 
+            Double x = 0;
+            
+            for (int i = 0; i < n + 1; i++)
+            {
+                xi[i] = x;
+                x += h;
+            }
+
+            //Заполним массивы 
+            //j = 0
+            fi[0] = ((-1) / 3) * Math.Pow(xi[1], 2) + (1/4) * Math.Pow(xi[1], 3);
+            ci[0] = (1 / Math.Pow(xi[1], 2)) * (2 * xi[1] + 0.25 * Math.Sin(2 * xi[1])) + xi[1] / 3;
+            bi[0] = (-1) * (1 / Math.Pow(xi[1], 2)) * (2 * xi[1] + 0.25 * Math.Sin(2 * xi[1])) + xi[1] / 6; 
+
+            // j = N
+            fi[n] = (-1 / 5) * Math.Pow(1 - xi[n - 1], 3) + Math.Pow(1 - xi[n - 1], 2) * ((1 / 4) + (1 / 2) * xi[n - 1])
+                + (1 - xi[n - 1]) * ((-2 / 3) * xi[n] - (1 / 3) * Math.Pow(xi[n], 2)) - (1 / 2) * Math.Pow(xi[n], 2);
+            ai[n] = ((-1) / (Math.Pow(1 - xi[n - 1], 2))) * (2 * (1 - xi[n - 1]) + (1 / 4) * Math.Sin(2 * (1 - xi[n - 1])))
+                + ((1 / (Math.Pow(1 - xi[n - 1], 2))) * ((-1) * (1 / 3) * Math.Pow(1 - xi[n - 1], 3) +
+                (1 / 2) * (1 + xi[n - 1]) * Math.Pow(1 - xi[n - 1], 2)
+                - xi[n - 1] * (1 - xi[n - 1])) + (2 + (1 / 2) * Math.Cos(2)));
+            ci[n] = ((-1) / (Math.Pow(1 - xi[n - 1], 2))) * (2 * (1 - xi[n - 1]) + (1 / 4) * Math.Sin(2 * (1 - xi[n - 1])))
+                + ((1 / (Math.Pow(1 - xi[n - 1], 2))) * ((1 / 3) * Math.Pow(1 - xi[n - 1], 3) - xi[n - 1] * Math.Pow(1 - xi[n - 1], 2)
+                + Math.Pow(xi[n - 1], 2) * (1 - xi[n - 1])) + (2 + (1 / 2) * Math.Cos(2)));
+            
+            //j = 1..N-1
+            for (int i = 1; i < n; i++) 
+            {
+                ai[i] = Cj1(xi[i - 1], xi[i]);
+                ci[i] = Cj(xi[i - 1], xi[i], xi[i + 1]);
+                bi[i] = Cj1(xi[i], xi[i + 1]);
+                fi[i] = Fj(xi[i - 1], xi[i], xi[i + 1]);
+            }
+
+            //Прогонка
+            solveMatrix(ai, ci, bi, fi);
+
+            //u = summ(Ci * si) i = 0...N
+
+            Double[] ui = new Double[n + 1];
+            for (int i = 0; i < n + 1; i++)
+            {
+                ui[i] = Ci[i] * si(xi[i], i);
+            }
+
+            //Строим график функции
+            GraphPane panel = zedGraphControl1.GraphPane;
+            panel.CurveList.Clear();
+
+            PointPairList u_list = new PointPairList();
+
+            // Устанавливаем интересующий нас интервал по оси X
+            panel.XAxis.Scale.Min = xi[0] - h;
+            panel.XAxis.Scale.Max = xi[n] + h;
+
+            for (int i = 0; i < xi.Count(); i++)
+            {
+                u_list.Add(xi[i], ui[i]);
+            }
+
+
+            LineItem Curve1 = panel.AddCurve("ui(x)", u_list, Color.Red, SymbolType.Star);
+
+            zedGraphControl1.AxisChange();
+            // Обновляем график
+            zedGraphControl1.Invalidate();
+          
+
+            //Таблица 
+            //Очистка строк и столбцов таблицы
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            //Заполнение
+            //Поскольку возможно вывести только 655 столбцов, то ограничим область видимости до 500 элементов
+            Int64 max_count = 0;
+            if (n > 500)
+            {
+                max_count = 500;
+            }
+            else
+                max_count = n;
+            dataGridView1.RowCount = (int)max_count + 1;
+            dataGridView1.ColumnCount = (int)max_count + 3;
+            for (int i = 0; i < (int)max_count + 1; i++)
+            {
+                for (int j = 0; j < (int)max_count + 1; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = 0;
+
+                    if (i == j)
+                        dataGridView1.Rows[i].Cells[j].Value = ci[i];
+                    if ((i != 0) && (i == j - 1))
+                        dataGridView1.Rows[i].Cells[j].Value = ai[i];
+                    if ((i != n ) && (i == j + 1))
+                        dataGridView1.Rows[i].Cells[j].Value = bi[i];
+
+                }
+
+                dataGridView1.Rows[i].Cells[(int)max_count + 2].Value = fi[i];
+            }
+
+            //Строим график коэффициентов 
+            GraphPane panel2 = zedGraphControl2.GraphPane;
+            panel2.CurveList.Clear();
+
+            PointPairList u_list2 = new PointPairList();
+
+            // Устанавливаем интересующий нас интервал по оси X (количество коэф)
+            panel2.XAxis.Scale.Min = 0;
+            panel2.XAxis.Scale.Max = n + 2;
+
+            for (int i = 0; i < Ci.Count(); i++)
+            {
+                u_list2.Add(i, Ci[i]);
+            }
+
+
+            LineItem Curve2 = panel2.AddCurve("Ci", u_list2, Color.Blue, SymbolType.Star);
+
+            zedGraphControl2.AxisChange();
+            // Обновляем график
+            zedGraphControl2.Invalidate();
+          
+
+        }
+
+
+        //Коэффициенты перед С_j1 //(j-1, j) and (j, j +1)
+        public Double Cj1(Double xj1, Double xj)
+        {
+            return ((-1) * (1 / Math.Pow(xj - xj1, 2)) * (2 * (xj - xj1) + 0.25 * Math.Sin(2 * (xj - xj1))  + (1 / 3) * Math.Pow(xj - xj1, 3)
+                - (1 / 2) * (xj + xj1) * Math.Pow(xj - xj1, 2) + xj1 * xj * (xj - xj1)) + (2 + (1 / 2) * Math.Cos(2) *
+                ((1 - xj ) * (1 - xj1) / ((xj - xj1) * (xj1 - xj)))));
+        }
+
+        //Коэффициенты перед С_j 
+        public Double Cj(Double xj1, Double xj, Double xj2)
+        {
+            return ((1 / Math.Pow(xj - xj1, 2)) * (2 * (xj - xj1) + (1 / 4) * Math.Sin(2 * (xj - xj1))) +
+                (1 / Math.Pow(xj2 - xj, 2)) * (2 * (xj2 - xj) + (1 / 4) * Math.Sin(2 * (xj2 - xj))) +
+                (1 / Math.Pow(xj - xj1, 2)) * ((1 / 3) * Math.Pow(xj - xj1, 3) - xj1 * Math.Pow(xj - xj1, 2) + Math.Pow(xj1, 2) * (xj - xj1)) +
+                (1 / Math.Pow(xj2 - xj, 2)) * ((1 / 3) * Math.Pow(xj2 - xj, 3) - xj2 * Math.Pow(xj2 - xj, 2) + Math.Pow(xj2, 2) * (xj2 - xj)) +
+                ((2 + (1 / 2) * Math.Cos(2)) * (1 - xj) * (((1 - xj1) / ((xj - xj1) * (xj1 - xj))) + ((1 - xj2) / ((xj - xj2) * (xj2 - xj))))));
+        }
+
+        //Правая часть для j = 1..N-1
+        public Double Fj(Double xj1, Double xj, Double xj2)
+        {
+            return ( (-1/5) * Math.Pow(xj2 - xj, 3) + (1/2) * xj2 * Math.Pow(xj2 - xj,2) + ((xj2 - xj) * (1/3 - 2/3 * xj2 - 1/3 * Math.Pow(xj2, 2))) + Math.Pow(xj2, 2)) 
+                + 
+                (-1/5) * Math.Pow(xj - xj1, 3) + (1/2) * xj1 * Math.Pow(xj - xj1,2) + ((xj - xj1) * (1/3 - 2/3 * xj1 - 1/3 * Math.Pow(xj1, 2))) + Math.Pow(xj1, 2) ;
+        }
+
+
+
+        //фи j (базис)
+        public Double si(Double x, Int64 i)
+        {
+            if (i == 0)
+            {
+                if (x >= xi[1])
+                    return 0;
+                return -x/xi[1] + 1;
+            }
+        
+            if (i == n)
+            {
+                if (x <= xi[n-1])
+                    return 0;
+                return (x - xi[n-1])/(1 - xi[n-1]);
+            }
+    
+            if ((x < xi[i-1]) || (x >= xi[i+1]))
+                return 0;
+            if (x < xi[i])
+                return (x - xi[i - 1]) / (xi[i] - xi[i - 1]);
+            if (x >= xi[i])
+                return (x - xi[i+1])/(xi[i] - xi[i+1]);
+    
+            return 0;
+ 
+        }
+
+        private void очистииьГрафикToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GraphPane panel = zedGraphControl1.GraphPane;
+            panel.CurveList.Clear();
+
+            zedGraphControl1.AxisChange();
+            // Обновляем график
+            zedGraphControl1.Invalidate();
+        }
+
+        private void очиститьВсеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            очистииьГрафикToolStripMenuItem_Click(sender, e);
+            очиститьТаблицуToolStripMenuItem_Click( sender,  e);
+            очиститьГрафикКоэффициэнтовToolStripMenuItem_Click(sender, e);
+
+        }
+
+        private void очиститьТаблицуToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Очистка строк и столбцов таблицы
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+        }
+
+        private void очиститьГрафикКоэффициэнтовToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GraphPane panel2 = zedGraphControl2.GraphPane;
+            panel2.CurveList.Clear();
+
+            zedGraphControl2.AxisChange();
+            // Обновляем график
+            zedGraphControl2.Invalidate();
+        }
+    }
+}
